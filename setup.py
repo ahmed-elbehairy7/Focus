5e37812df0fb277558b9e95363d166d5d39d5ccb
Ahmed Elbehairy <ahmedelbehairy.72005@gmail.com>
2023-12-28 18:31:37 +0200

    Added pausing feature

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from pam import BaseApp
 
-focus = BaseApp('focus.io', '01.00.02', 'Productivity', 'Timer', date="19/3/1445", application_nu=0)
+focus = BaseApp('focus.io', '01.01.00', 'Productivity', 'Timer', date="19/3/1445", application_nu=0)
 
 if __name__=="__main__":
     print('\n\n')
-    BaseApp.printApplication()
-    BaseApp.printApps()
+    focus.printApplication()
+    focus.printApps()
