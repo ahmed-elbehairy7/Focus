a8a0fa05599cd2057686a20e1e6b02f670c0e92c
Ahmed Elbehairy <ahmedelbehairy.72005@gmail.com>
2024-02-18 08:22:04 +0200

    Solved some bugs

--- main.py
+++ main.py
@@ -12,110 +12,116 @@ from inputimeout import inputimeout, TimeoutOccurred
 # ----------------MAIN FUNCTION------------------#
 # ----------------MAIN FUNCTION------------------#
 
+SAVED_TASKS = r"D:\\pam\\focus.io\\saved.json"
+DO = r"D:\\pam\\focus.io\\.do"
+
+
 def main():
-    
-    #Take those global variables to allow editing them for making custom sized progress bar for each task for better visualization
+    # Take those global variables to allow editing them for making custom sized progress bar for each task for better visualization
     global terminal, indent, bar
 
-    #Get the size of the terminal, progress bar, and the indentation before printing the progress bar
+    # Get the size of the terminal, progress bar, and the indentation before printing the progress bar
     terminal, indent, bar = get_terminal_data()
-	
-    #print an intro for the application
+
+    # print an intro for the application
     # print(focus)
-    
-    #Make the computer say the following
+
+    # Make the computer say the following
     speak("Welcome to FOCUS.io")
-    
+
     Task.get_tasks("Please enter the one time tasks", True)
     Task.get_tasks("Please enter the looping tasks")
-    
-    #Forever:
+
+    # Forever:
     while True:
-	#For each task added by the user
+        # For each task added by the user
         for task in Task.tasks:
-	    
             task.exec()
 
         Task.congrats()
         Task.filter_tasks()
 
 
-
 class Task:
-
     tasks = []
 
-    def __init__(self, name : str, duration : int, one_time : bool = False, msg = None):
+    def __init__(self, name: str, duration: int, one_time: bool = False, msg=None):
         self.name = name
         self.duration = duration
         self.one_time = one_time
-        
+
         if not msg:
             self.msg = f"Now is {self.name} time, you will have to do it for {self.duration} minutes"
-        else: 
+        else:
             self.msg = msg
 
         Task.tasks.append(self)
 
     def exec(self):
-        
-        #Get the terminal size, progressbar, indentation again so the program is sure that the progress bar is pretty printed even if the user changed the size of the window
-        terminal, indent, bar = get_terminal_data()
-    
-        #Get the indentation for the message printed for the task so it can be middle aligned
+        # Get the terminal size, progressbar, indentation again so the program is sure that the progress bar is pretty printed even if the user changed the size of the window
+        terminal, _, bar = get_terminal_data()
+
+        # Get the indentation for the message printed for the task so it can be middle aligned
         side_space = int((terminal - len(self.msg)) / 2)
-    
-        #Print the message of the task middle aligned and say it loud so the user can hear
+
+        # Print the message of the task middle aligned and say it loud so the user can hear
         print("\n\n")
         print(" " * side_space, self.msg, sep="")
         speak(self.msg)
 
-        #The function that track time and print the progress for the task
+        # The function that track time and print the progress for the task
         progressbar(self.duration * 60 / bar)
-       
-    
+
     def __str__(self) -> str:
-        return f"name: {self.name}, duration: {self.duration}, msg: {self.msg}, one time: {self.one_time}" 
-    
+        return f"name: {self.name}, duration: {self.duration}, msg: {self.msg}, one time: {self.one_time}"
+
     @classmethod
     def save(cls):
         with open("tasks.json", "w") as file:
-            dump(list(map(lambda task: {
-                "name": task.name,
-                "duration": task.duration,
-                "msg": task.msg
-            }, cls.tasks)), file, indent=4)
-    
+            dump(
+                list(
+                    map(
+                        lambda task: {
+                            "name": task.name,
+                            "duration": task.duration,
+                            "msg": task.msg,
+                        },
+                        cls.tasks,
+                    )
+                ),
+                file,
+                indent=4,
+            )
+
     @classmethod
     def filter_tasks(cls):
-        cls.tasks = filter(lambda x : x.one_time == False, cls.tasks)
-        
+        cls.tasks = filter(lambda x: x.one_time == False, cls.tasks)
+
     @classmethod
     def congrats(cls):
-        #Congrat the user whenever he finishes a whole set of tasks by pretty printing this message middle aligned and saying it
+        # Congrat the user whenever he finishes a whole set of tasks by pretty printing this message middle aligned and saying it
         cong = "Congratulations, you had just completed a whole loop!"
         l = int((terminal - len(cong)) / 2)
         print(" " * l, cong, sep="")
         speak(cong)
-    
+
     @classmethod
-    def get_tasks(cls, msg : str, one_time : bool = False) -> None:
+    def get_tasks(cls, msg: str, one_time: bool = False) -> None:
         while True:
             print(msg)
             speak(msg)
-            
-            #The function that takes the tasks from the user
+
+            # The function that takes the tasks from the user
             get_details(one_time)
             break
-        
-        
-        
-def get_details(one_time : bool):
+
+
+def get_details(one_time: bool):
     count = 1
     while True:
-        task = inputt(f"{count}: ", 600)
+        task = inputt(f"{count}: ", 5)
         match task:
-            case "R" :
+            case "R":
                 Task.tasks = []
                 count = 1
                 continue
@@ -126,90 +132,94 @@ def get_details(one_time : bool):
             case _:
                 count += 1
                 pass
-        if not saved(task, one_time):
+        if one_time or not saved(task, one_time):
             Task(task, get_duration(), one_time)
-    
-        
-def progressbar(sleeping):
 
+
+def progressbar(sleeping):
     print(" " * indent, "_" * bar, sep="")
     print(" " * (indent - 1), "[", sep="", end="")
 
     for _ in range(bar):
-        
-        with open(".do", 'r+') as f:
+        with open(DO, "r+") as f:
             do = f.read()
             f.truncate(0)
             match do:
-                case "p": 
-                    inputt("", 1)
+                case "p":
+                    inputt("", 10)
                     speak("You are having break for ten minutes now! go back to work")
-                case "P" : inputt("", 30)
-                case "f": return
-                case _: sleep(sleeping)
-      
+                case "P":
+                    inputt("", 30)
+                case "f":
+                    return
+                case _:
+                    sleep(sleeping)
+
         stdout.write("=")
         stdout.flush()
 
     print("]\n", " " * indent, "_" * bar, "\n\n", sep="")
-    
 
-def saved(shortcut : str, one_time : bool):
-    with open("saved.json") as file:
+
+def saved(shortcut: str, one_time: bool):
+    with open(SAVED_TASKS) as file:
         saved_data = load(file)
-    
+
     if shortcut in saved_data:
-        duration = saved_data[shortcut]['duration']
+        duration = saved_data[shortcut]["duration"]
         print(f"duration is set as defult: {duration}")
     elif shortcut.lower() in saved_data:
         duration = get_duration()
     else:
         return
-    
+
     obj = saved_data[shortcut.lower()]
-    Task(obj['name'], duration, one_time, obj['msg'])
+    Task(obj["name"], duration, one_time, obj["msg"])
     return True
+
 
 def get_duration():
     while True:
         try:
-            return int(inputt("Duration: ", 100))
+            return int(inputt("Duration: ", 1))
         except ValueError:
             continue
 
+
 def get_terminal_data():
     terminal: int = int(
-    str(get_terminal_size()).replace("os.terminal_size(columns=", "").split(",")[0]
-)
+        str(get_terminal_size()).replace("os.terminal_size(columns=", "").split(",")[0]
+    )
     indent: int = int(terminal / 25)
     bar: int = int((terminal / 25) * 23)
     return terminal, indent, bar
-        
 
-def inputt(prompt : str, n : int = 15 ) -> None:
+
+def inputt(prompt: str, n: int = 15) -> None:
     try:
-        return inputimeout(prompt=prompt, timeout=n)
+        return inputimeout(prompt=prompt, timeout=n * 60)
     except TimeoutOccurred:
         return
-           
-def edit_file(key : str) -> None:
+
+
+def edit_file(key: str) -> None:
     with open(".do", "w") as f:
         f.write(key)
+
 
 terminal, indent, bar = None, None, None
 
 
 if __name__ == "__main__":
-        try:
-            main()
-        except (KeyboardInterrupt, SystemExit) as e:
-            exit()
-        except BaseException as e:
-            print_exc()
-            match inputt("Press enter to search, any key to exit: "):
-                case "":
-                    
-                    exit()
-                case _:
-                    popen(f"search {e}")
-                    exit()
+    try:
+        main()
+    except (KeyboardInterrupt, SystemExit) as e:
+        exit()
+    except BaseException as e:
+        print_exc()
+        match inputt("Press enter to search, any key to exit: "):
+            case "":
+                exit()
+            case _:
+                popen(f"search {e}")
+                exit()
--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from pam import BaseApp
 
-focus = BaseApp('focus.io', '01.01.01', 'Productivity', 'Timer', date="19/3/1445", application_nu=0, wanted_extension='')
+focus = BaseApp(
+    "focus.io",
+    "02.00.00",
+    "Productivity",
+    "Timer",
+    date="19/3/1445",
+    application_nu=0,
+    wanted_extension="",
+)
 
-if __name__=="__main__":
-    print('\n\n')
+if __name__ == "__main__":
+    print("\n\n")
     focus.printApplication()
     focus.printApps()
