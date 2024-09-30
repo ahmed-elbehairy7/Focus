033497141da8b235557b5cfc0497c91fc0b4d248
Ahmed Elbehairy <ahmedelbehairy.72005@gmail.com>
2024-09-30 15:13:46 +0300

    fixed duplicate commands bug

--- src/command.py
+++ src/command.py
@@ -5,68 +5,61 @@ from time import sleep
 from exceptions import SkipTask
 
 
-def pauseFunc(t : int):
-        while True:
-            if 'r' == Command.queue.get():
-                Command.checkCommand(t)
-                return
+def pauseFunc(t: int):
+    while True:
+        if "r" == Command.queue.get():
+            Command.checkCommand(t)
+            return
+
+
 class Command:
     commandObjects = [
+        {"letter": "p", "help": "pause task progress", "func": pauseFunc},
+        {"letter": "r", "help": "resume task progress", "func": lambda t: sleep(t)},
         {
-            "letter" : "p",
-            "help" : "pause task progress",
-            "func" : pauseFunc 
+            "letter": "s",
+            "help": "skip task",
+            "func": lambda _: (_ for _ in ()).throw(SkipTask()),
         },
         {
-            "letter" : "r",
-            "help" : "resume task progress",
-            "func" : lambda t : sleep(t)
+            "letter": "e",
+            "help": "exit the application",
+            "func": lambda _: (_ for _ in ()).throw(SystemExit()),
         },
-        {
-            "letter" : "s",
-            "help" : "skip task",
-            "func" :lambda _ : (_ for _ in ()).throw(SkipTask())
-        },
-        {
-            "letter" : "e",
-            "help" : "exit the application",
-            "func" : lambda _ : (_ for _ in ()).throw(SystemExit())
-        }
     ]
-    
+
     queue = Queue()
-    commands  = []
-    def __init__(self, letter : str, help : str, func) -> None:
+    commands = []
+
+    def __init__(self, letter: str, help: str, func) -> None:
         self.letter = letter
         self.help = help
         self.func = func
-        on_press_key(self.letter, lambda _ : self.send())
+        on_press_key(self.letter, lambda _: self.send())
         Command.commands.append(self)
-        
-        
-    
+
     def send(self):
-        title : str = getActiveWindowTitle()
+        title: str = getActiveWindowTitle()
         if "focus.exe" in title or "focus.io" in title:
             Command.queue.put(self.letter)
-            
+
     def __str__(self) -> str:
         return f"({self.letter})    {self.help}"
-            
+
     @classmethod
     def initialize_commands(cls):
-        for command in cls.commandObjects:
-            Command(**command)
-            
-            
+        if not len(cls.commands):
+            for command in cls.commandObjects:
+                Command(**command)
+
     @classmethod
     def print_help(cls):
         print("Available commands: ")
         for command in cls.commands:
             print(command)
-    
+
     @classmethod
-    def checkCommand(cls, sleeping : int):
+    def checkCommand(cls, sleeping: int):
         try:
             t = Command.queue.get(True, sleeping)
             for command in cls.commands:
@@ -74,6 +67,5 @@ class Command:
                     command.func(sleeping)
                     return
             sleep(sleeping)
-        except Empty :
+        except Empty:
             pass
-        
